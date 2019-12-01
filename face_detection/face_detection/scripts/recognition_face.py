f2bfcb9fd583942fdec91021913bbb8b0e984e31
Wilfred Vasquez <wilfredvas@gmail.com>
2019-12-01 02:13:48 -0400

    Inserción de la red roman

--- face_detection/face_detection/scripts/recognition_face.py
+++ face_detection/face_detection/scripts/recognition_face.py
@@ -1,18 +1,28 @@
 import cv2
 import os
+import dlib
+import cvlib as cv
 from django.conf import settings
 from threading import Thread
 from face_detection.app.models import Cameras
-from utils.prediction import prediction
+from utils.prediction import (
+    prediction, prediction_fd, landmarks_to_np, get_centers, get_aligned_face,
+    judge_eyeglass)
 from utils.camera import CameraStream
 
 def run():
-    size = 5
+    size = 4
     (im_width, im_height) = (112, 92)
     
     # Cargo el archivo de configuración para la detección de rostros
-    xml_file = os.path.join(settings.APPS_DIR, 'fixtures/haarcascade_frontalface_default.xml')
+    xml_file = os.path.join(settings.APPS_DIR,
+        'fixtures/haarcascade_frontalface_default.xml')
     face_cascade = cv2.CascadeClassifier(xml_file)
+    predictor_eye_path = os.path.join(settings.APPS_DIR,
+        'fixtures/shape_predictor_5_face_landmarks.dat')
+    detector = dlib.get_frontal_face_detector()
+    predictor_eye = dlib.shape_predictor(predictor_eye_path)
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 
     # Obtengo de la base de datos las cámaras detectadas y activas
     cameras = list()
@@ -27,32 +37,79 @@ def run():
 
     while True:
         for cap in cameras:
-                frame = cap["src"].read()            
+                frame = cap["src"].read()         
                 #frame=cv2.flip(frame,1,0)
 
                 #convertimos la imagen a blanco y negro    
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
                 #redimensionar la imagen
-                mini = cv2.resize(gray, (int(gray.shape[1] / size), int(gray.shape[0] / size)))
+                mini = cv2.resize(frame, (int(frame.shape[1] / size), int(frame.shape[0] / size)))
 
                 """buscamos las coordenadas de los rostros (si los hay) y
                 guardamos su posicion"""
-                faces = face_cascade.detectMultiScale(mini)
-                for i in range(len(faces)):
-                    face_i = faces[i]
-                    (x, y, w, h) = [v * size for v in face_i]
-                    face = gray[y:y + h, x:x + w]
-                    face_resize = cv2.resize(face, (im_width, im_height))
+                faces, confidence = cv.detect_face(mini)
+                for i, f in enumerate(faces):
+                    (startX, startY) = size*f[0], size*f[1]
+                    (endX, endY) = size*f[2], size*f[3]
+
+                    face = gray[startY:endY,startX:endY]
+                    face_ok = False
+                    try:
+                        face_ok = True if (len(face[0]) > 0 and len(face) > 0) else False
+                    except:
+                        pass
+
+                    if face_ok:
+                        face_resize = cv2.resize(face, (im_width, im_height))
+                        
+                        #*****************************Reconocimiento de lentes***********************************
+                        rects = detector(gray, 1)
+
+                        for i, rect in enumerate(rects):
+                            x_face = rect.left()
+                            y_face = rect.top()
+                            w_face = rect.right() - x_face
+                            h_face =  rect.bottom() - y_face
                     
-                    #Dibujamos un rectangulo en las coordenadas del rostro
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                            landmarks = predictor_eye(gray,rect)
+                            landmarks = landmarks_to_np(landmarks)
+                            for (x, y) in landmarks:
+                                cv2.circle(frame, (x, y), 2, (0, 0, 255), -1)
+                
+                            LEFT_EYE_CENTER, RIGHT_EYE_CENTER = get_centers(frame, landmarks)
                     
-                    # Se realiza la predección
-                    prediction(face_resize, cap["camera_id"], frame)
+                            aligned_face = get_aligned_face(gray, LEFT_EYE_CENTER, RIGHT_EYE_CENTER)
                     
+                            judge = judge_eyeglass(aligned_face)
+                            if judge == True:
+                                cv2.putText(frame, "Usando Lentes",
+                                    (x_face + 100, y_face - 10),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2, cv2.LINE_AA)
+                            else:
+                                continue
+
+
+                        # Se realiza la predección
+                        #prediction(face_resize, cap["camera_id"], frame)
+                        name, prediction = prediction_fd(face_resize, cap["camera_id"], frame)
+
+                        cara = '%s' % (name)
+                        text = "{:.2f}%".format(confidence[i]*100)
+                        if prediction[1]<=130: #Rostro identificado
+            #                #Dibujamos un rectangulo en las coordenadas del rostro
+            #            #Ponemos el nombre de la persona que se reconoció
+                            cv2.rectangle(frame, (startX, startY), (endX,endY), (0,255,0), 3)
+                            cv2.putText(frame,'%s' % (cara),(startX+20,startY-10),cv2.FONT_HERSHEY_PLAIN,2,(0, 255, 255))
+                            cv2.putText(frame,text,(endX-20,startY-10),cv2.FONT_HERSHEY_PLAIN,2,(0, 255, 255))
+                              
+            #    #        #Si la prediccion es mayor a 100 no es un reconomiento con la exactitud suficiente
+                        else:#if #prediction[1]>101 and prediction[1]<500: 
+                            cv2.rectangle(frame, (startX, startY), (endX,endY), (0,0,255), 3)
+                            cv2.putText(frame, 'Desconocido',(startX+20,startY-10), cv2.FONT_HERSHEY_PLAIN,1,(0,0,255))
+                            
                     #Mostramos la imagen
-                #cv2.imshow('OpenCV Reconocimiento facial', frame)
+                cv2.imshow('OpenCV Reconocimiento facial', frame)
 
         #Si se presiona la tecla ESC se cierra el programa
         key = cv2.waitKey(10)
--- face_detection/face_detection/utils/prediction.py
+++ face_detection/face_detection/utils/prediction.py
@@ -1,7 +1,8 @@
 import cv2
 import os
+import dlib
 import base64
-import numpy
+import numpy as np
 from django.conf import settings
 from face_detection.app.tasks import set_notifications
 
@@ -22,7 +23,7 @@ def prediction(face_resize, camera_id, frame):
             id += 1
     
     # Crear una matriz Numpy de las dos listas anteriores
-    (images, lables) = [numpy.array(lis) for lis in [images, lables]]
+    (images, lables) = [np.array(lis) for lis in [images, lables]]
     # OpenCV entrena un modelo a partir de las imagenes
     model = cv2.face.LBPHFaceRecognizer_create()
     model.train(images, lables)
@@ -36,4 +37,139 @@ def prediction(face_resize, camera_id, frame):
         retval, buffer = cv2.imencode('.png', frame)
         png_as_text = base64.b64encode(buffer).decode('ascii')
         set_notifications(name, camera_id, png_as_text)
+
+def prediction_fd(face_resize, camera_id, frame):
+    # Crear una lista de imagenes y una lista de nombres correspondientes
+    dir_faces = os.path.join(settings.MEDIA_ROOT, 'att_faces/orl_faces')
+    (images, labels, names, id) = ([], [], {}, 0)
+    print('Procesando base de datos...')
+    for (subdirs, dirs, files) in os.walk(dir_faces):
+        for subdir in dirs:
+            names[id] = subdir
+            subjectpath = os.path.join(dir_faces, subdir)
+            for filename in os.listdir(subjectpath): 
+                if filename!='Thumbs.db':
+                    path = subjectpath + '/' + filename
+                   
+                    label = id
+                    images.append(cv2.imread(path, 0))
+                    labels.append(int(label))
+                else:
+                    continue
+            id += 1
+    print('Fin del procesamiento...')
+    (im_width, im_height) = (112, 92)
+    # Crear una matriz Numpy de las dos listas anteriores
+    (images, labels) = [np.array(lis) for lis in [images, labels]]
+    # OpenCV entrena un modelo a partir de las imagenes
+    model = cv2.face.LBPHFaceRecognizer_create()
+    model.train(images, labels)
+
+    prediction = model.predict(face_resize)
+
+    cara = '%s' % (names[prediction[0]])
+    name = cara if prediction[1]<130 else "Desconocido"
+    if prediction[1]<=500: #Rostro identificado
+        retval, buffer = cv2.imencode('.png', frame)
+        png_as_text = base64.b64encode(buffer).decode('ascii')
+        set_notifications(name, camera_id, png_as_text)
+
+    return names[prediction[0]], prediction
+
+def landmarks_to_np(landmarks, dtype="int"):
     
+        num = landmarks.num_parts
+        
+        # initialize the list of (x, y)-coordinates
+        coords = np.zeros((num, 2), dtype=dtype)
+        
+        # loop over the 68 facial landmarks and convert them
+        # to a 2-tuple of (x, y)-coordinates
+        for i in range(0, num):
+            coords[i] = (landmarks.part(i).x, landmarks.part(i).y)
+        # return the list of (x, y)-coordinates
+        return coords
+
+def get_centers(img, landmarks):
+        EYE_LEFT_OUTTER = landmarks[2]
+        EYE_LEFT_INNER = landmarks[3]
+        EYE_RIGHT_OUTTER = landmarks[0]
+        EYE_RIGHT_INNER = landmarks[1]
+    
+        x = ((landmarks[0:4]).T)[0]
+        y = ((landmarks[0:4]).T)[1]
+        A = np.vstack([x, np.ones(len(x))]).T
+        k, b = np.linalg.lstsq(A, y, rcond=None)[0]
+        
+        x_left = (EYE_LEFT_OUTTER[0]+EYE_LEFT_INNER[0])/2
+        x_right = (EYE_RIGHT_OUTTER[0]+EYE_RIGHT_INNER[0])/2
+        LEFT_EYE_CENTER =  np.array([np.int32(x_left), np.int32(x_left*k+b)])
+        RIGHT_EYE_CENTER =  np.array([np.int32(x_right), np.int32(x_right*k+b)])
+        
+        pts = np.vstack((LEFT_EYE_CENTER,RIGHT_EYE_CENTER))
+        cv2.polylines(img, [pts], False, (255,0,0), 1) #画回归线
+        cv2.circle(img, (LEFT_EYE_CENTER[0],LEFT_EYE_CENTER[1]), 3, (0, 0, 255), -1)
+        cv2.circle(img, (RIGHT_EYE_CENTER[0],RIGHT_EYE_CENTER[1]), 3, (0, 0, 255), -1)
+        
+        return LEFT_EYE_CENTER, RIGHT_EYE_CENTER
+
+def get_aligned_face(img, left, right):
+        desired_w = 256
+        desired_h = 256
+        desired_dist = desired_w * 0.5
+        
+        eyescenter = ((left[0]+right[0])*0.5 , (left[1]+right[1])*0.5)# 眉心
+        dx = right[0] - left[0]
+        dy = right[1] - left[1]
+        dist = np.sqrt(dx*dx + dy*dy)# 
+        scale = desired_dist / dist # 
+        angle = np.degrees(np.arctan2(dy,dx)) #
+        M = cv2.getRotationMatrix2D(eyescenter,angle,scale)# 
+    
+        # update the translation component of the matrix
+        tX = desired_w * 0.5
+        tY = desired_h * 0.5
+        M[0, 2] += (tX - eyescenter[0])
+        M[1, 2] += (tY - eyescenter[1])
+    
+        aligned_face = cv2.warpAffine(img,M,(desired_w,desired_h))
+        
+        return aligned_face
+
+def judge_eyeglass(img):
+        img = cv2.GaussianBlur(img, (11,11), 0) 
+    
+        sobel_y = cv2.Sobel(img, cv2.CV_64F, 0 ,1 , ksize=-1) 
+        sobel_y = cv2.convertScaleAbs(sobel_y) 
+    
+        edgeness = sobel_y 
+        
+        retVal,thresh = cv2.threshold(edgeness,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        
+        d = len(thresh) * 0.5
+        x = np.int32(d * 6/7)
+        y = np.int32(d * 3/4)
+        w = np.int32(d * 2/7)
+        h = np.int32(d * 2/4)
+    
+        x_2_1 = np.int32(d * 1/4)
+        x_2_2 = np.int32(d * 5/4)
+        w_2 = np.int32(d * 1/2)
+        y_2 = np.int32(d * 8/7)
+        h_2 = np.int32(d * 1/2)
+        
+        roi_1 = thresh[y:y+h, x:x+w] 
+        roi_2_1 = thresh[y_2:y_2+h_2, x_2_1:x_2_1+w_2]
+        roi_2_2 = thresh[y_2:y_2+h_2, x_2_2:x_2_2+w_2]
+        roi_2 = np.hstack([roi_2_1,roi_2_2])
+        
+        measure_1 = sum(sum(roi_1/255)) / (np.shape(roi_1)[0] * np.shape(roi_1)[1])
+        measure_2 = sum(sum(roi_2/255)) / (np.shape(roi_2)[0] * np.shape(roi_2)[1])
+        measure = measure_1*0.3 + measure_2*0.7
+ 
+    
+        if measure > 0.15:
+            judge = True
+        else:
+            judge = False
+        return judge
